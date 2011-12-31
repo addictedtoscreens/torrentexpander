9e84459859b404fc04bd87eea5c58ecf1641d6a5
addictedtoscreens@gmail.com <addictedtoscreens@gmail.com@1bfae07b-60de-1506-f44b-f34c82a265cd>
2011-12-31 01:21:53 +0000

    Added url removal in filename. Only .com domains are detected for now

--- torrentexpander.sh
+++ torrentexpander.sh
@@ -100,7 +100,7 @@ music_extensions="mp3,m4a,wav"
 movies_detect_patterns="HDTV,DVDRip,BDRip,BRRip,DVDR,720p,1080p,HD1080p"
 movies_detect_patterns_pt_2="TS,TVRip,DVDSCR,R5,Workprint,SCR,Screener,HDRip,DVDScreener"
 other_movies_patterns="proper,repack,rerip,pdtv,hdtv,xvid,webrip,web-dl,readnfo,ntsc,pal,limited,ws,uncut,unrated,internal,480p,festival,bluray,extended,italian,theatrical.cut,dubbed,collection,remastered,season,nlsubs,spanish,divx,x264,hdtvrip,dvdriptorrents"
-scene_patterns="aaf"
+scene_patterns="[. _-]*www[. _-].*[. _-]com[. _-]*,aaf"
 audio_quality_patterns="AC3,DTS,LiNE,CAM AUDIO,MD,LD,Studio Audio"
 ####################### Optional functionalities variables #######################
 #################### Set these variables to "no" to disable ######################
